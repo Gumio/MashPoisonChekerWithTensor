59dab08df0a2bf6480d4e09242423cfb09abac56
Gumio <quran.gumi3134@gmail.com>
2018-01-15 17:32:39 +0900

    add splash

--- app/src/main/java/org/tensorflow/demo/SplashActivity.java
+++ app/src/main/java/org/tensorflow/demo/SplashActivity.java
@@ -1,25 +1,36 @@
 package org.tensorflow.demo;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
+import org.tensorflow.demo.databinding.ActivitySplashBinding;
+
 public class SplashActivity extends AppCompatActivity {
+
+    ActivitySplashBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_splash);
+
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "kin.ttf");
+        mBinding.textView.setTypeface(typeface);
+
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent intent = new Intent(getApplicationContext(), ClassifierActivity.class);//画面遷移のための    　　　　　　Intentを準備
-                startActivity(intent);//実際の画面遷移を開始
-                finish();//現在のActivityを削除
+                startActivity(intent);
+                finish();
             }
-        }, 2000);//2秒後にrun()を行う
+        }, 2000);
 
     }
 }
